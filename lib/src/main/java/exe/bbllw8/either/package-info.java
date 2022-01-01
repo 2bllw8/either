2d57e4d2f32b45b6ee7f2dc603f1a1ec0b648878
2bllw8 <2bllw8@gmail.com>
2022-01-01 17:01:05 +0100

    Add package-info.java

---
+++ lib/src/main/java/exe/bbllw8/either/package-info.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 2bllw8
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * This package provides the Either type.
+ * <p>
+ * Either is a type that represents a value
+ * of one of two possible types (disjoint union).
+ * An instance of {@link exe.bbllw8.either.Either} is
+ * an instance of <i>either</i> {@link exe.bbllw8.either.Left}
+ * or {@link exe.bbllw8.either.Right}.
+ * </p>
+ *
+ * @author 2bllw8
+ * @since 1.0.0
+ */
+package exe.bbllw8.either;
